7eb6f5e90814b363b7c8e0ab5f015d9cf2410276
Omolewu Sunday <omolewu12@gmail.com>
2022-06-21 13:30:51 +0100

    Login page

--- server.php
+++ server.php
@@ -1,6 +1,6 @@
 <?php
 require_once "database.php";
-
+$login_Errors = [];
 $name = $email = $nameErr = $emailErr = "";
 if (isset($_POST['register'])) {
     if (empty($_POST['name'])) {
@@ -23,4 +23,30 @@ if (isset($_POST['register'])) {
             echo "Success";
         }
     }
+}
+
+if (isset($_POST['login'])) {
+    if (empty($_POST['email'])) {
+        $login_Errors['emailErr'] = "Your Email is required";
+    } else {
+        $email = $_POST['email'];
+    }
+    if (empty($_POST['password'])) {
+        $login_Errors['passwordErr'] = "Your password is required";
+    } else {
+        $password = md5($_POST['password']);
+    }
+
+    if (!$login_Errors) {
+        $sql = "SELECT * FROM registration WHERE email='$email' AND password='$password'";
+        $select = mysqli_query($db, $sql);
+        if (mysqli_num_rows($select) > 0) {
+            $result = mysqli_fetch_array($select);
+            echo $result['name'];
+            echo  $result['email'];
+        } else {
+            $login_Errors['userErr'] = "Invalid details";
+        }
+    }
+    print_r($login_Errors);
 }
